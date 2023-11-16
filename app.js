36c88db2422c130c639088854919028fd566fd18
DamianUduevbo <58115973+DamianUduevbo@users.noreply.github.com>
2023-11-16 17:13:04 -0500

    lower case imports

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 import express from "express";
-import Hello from "./hello.js";
-import Lab5 from "./lab5.js";
+import hello from "./hello.js";
+import lab5 from "./lab5.js";
 import cors from "cors";
-import CourseRoutes from "./courses/routes.js";
-import ModuleRoutes from "./Modules/routes.js";
+import courseroutes from "./courses/routes.js";
+import moduleroutes from "./Modules/routes.js";
 import "dotenv/config";
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-ModuleRoutes(app);
-CourseRoutes(app);
-Lab5(app);
-Hello(app);
+moduleroutes(app);
+courseroutes(app);
+lab5(app);
+hello(app);
 
 app.listen(process.env.PORT || 4000);
